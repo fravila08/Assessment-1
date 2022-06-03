9bfee7abc06849f071e5eeeec1b801461fd49038
fravila08 <105952966+fravila08@users.noreply.github.com>
2022-06-03 12:29:38 -0700

    COMPLETE

--- optimal_change.py
+++ optimal_change.py
@@ -1,1 +1,44 @@
-# Write your solution here!
+import math
+def optimal_change(item_cost, amount_paid):
+    #Build a directory with money*100 to avoid float numbers
+    dollars={
+        '$100 bill':10000,
+        '$50 bill':5000,
+        '$20 bill':2000,
+        '$10 bill':1000,
+        '$5 bill':500,
+        '$1 bill':100,
+        'quarter':25,
+        'dime':10,
+        'nickle':5,
+        'pennie':1,
+    }
+    paid=math.floor(amount_paid*100)
+    cost=math.floor(item_cost*100)
+    change=paid - cost
+    #Don't want any floating numbers 
+    
+    answer=f"The optimal change for an item that costs ${item_cost} with an amount paid of ${amount_paid} is "
+    #if change is only one pennie, get rid of the 'and' in your statement
+    if change==1:
+        answer+='1 pennie.'
+    else:
+    #iterate through the values of the directory subtracting the corresponding value from change and adding the correct keys to our answer
+        for value in dollars:
+            counter=0
+            while change>=dollars[value]:
+                change-=dollars[value]
+                counter+=1
+            #before adding it to answer determine whether theres 1 or more times this value is being pushed so you could add the 's' and if it's pennies so you could switch the '.' for a ','
+            if counter>1:
+                if value=='pennie':
+                    answer+=f"and {counter} {value}s."
+                else:
+                    answer+=f"{counter} {value}s, "
+            elif counter== 1:
+                if value=='pennie':
+                    answer+=f"and 1 {value}."
+                else:
+                    answer+=f"1 {value}, "
+    return answer
+    
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,1 +1,11 @@
-# Write your tests here!
+from optimal_change import optimal_change
+#The following Test cases should return true
+print("1:", optimal_change(62.13, 100)=="The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
+print("2:", optimal_change(31.51,50)=="The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
+print("3:", optimal_change(99.99,100)=="The optimal change for an item that costs $99.99 with an amount paid of $100 is 1 pennie.")
+print("4:", optimal_change(53.27,250)=="The optimal change for an item that costs $53.27 with an amount paid of $250 is 1 $100 bill, 1 $50 bill, 2 $20 bills, 1 $5 bill, 1 $1 bill, 2 quarters, 2 dimes, and 3 pennies.")
+print("5:", optimal_change(23.17,97.99)=="The optimal change for an item that costs $23.17 with an amount paid of $97.99 is 1 $50 bill, 1 $20 bill, 4 $1 bills, 3 quarters, 1 nickle, and 2 pennies.")
+print("6:", optimal_change(37.78,40)=="The optimal change for an item that costs $37.78 with an amount paid of $40 is 2 $1 bills, 2 dimes, and 2 pennies.")
+print("7:", optimal_change(23.17,97.99)=="The optimal change for an item that costs $23.17 with an amount paid of $97.99 is 1 $50 bill, 1 $20 bill, 4 $1 bills, 3 quarters, 1 nickle, and 2 pennies.")
+print("8:", optimal_change(5.07,100)=="The optimal change for an item that costs $5.07 with an amount paid of $100 is 1 $50 bill, 2 $20 bills, 4 $1 bills, 3 quarters, 1 dime, 1 nickle, and 3 pennies.")
+
